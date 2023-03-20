e367aeef2fa559e66bc681d5263b4e0295c7e918
ayman mansour <am5432@fayoum.edu.eg>
2023-03-20 23:38:28 +0200

    orignal

---
+++ lab3_assignment/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("First Application"),
+        backgroundColor: Colors.black54,
+      ),
+      body: const Center(child: Text("Ayman Mansour")),
+    ),
+  ));
+}
